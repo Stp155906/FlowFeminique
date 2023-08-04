8a2f82b5304874670a45a6fbf57df4d372328cd1
Shantalia <71754706+Stp155906@users.noreply.github.com>
2023-08-04 15:43:50 -0400

    Initial Commit

---
+++ SynergeticRituals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SynergeticRituals
+//
+//  Created by Shantalia Z on 8/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SynergeticRituals/SynergeticRitualsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynergeticRitualsApp.swift
+//  SynergeticRituals
+//
+//  Created by Shantalia Z on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SynergeticRitualsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
